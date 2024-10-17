c1cdbd89152e9729e9f4dee8ba94ab7497cc6730
shinithp <shinithp78@gmail.com>
2024-10-17 14:06:23 +0530

    changed to a  structured folder and loginRegister updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,14 +21,14 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select(-password);
         if (!user) return res.status(404).send('User not found');
     
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(401).send('Invalid credentials');
     
         const token = jwt.sign({ id: user._id }, 'JWT_SECRET');
-        res.json({ token }); 
+        res.json({ token, user }); 
     } catch (error) {
         console.log(error)
     }
